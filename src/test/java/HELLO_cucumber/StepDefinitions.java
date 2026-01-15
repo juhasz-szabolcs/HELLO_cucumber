572ed2f33595a2f6f71489cd8f9130e058414533
Szabolcs Juhász <183376403+juhasz-szabolcs@users.noreply.github.com>
2026-01-15 21:25:33 +0100

    "Nope"

--- src/test/java/HELLO_cucumber/StepDefinitions.java
+++ src/test/java/HELLO_cucumber/StepDefinitions.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class IsItFriday {
     static String isItFriday(String today) {
-        return null;
+        return "Nope";
     }
 }
 
