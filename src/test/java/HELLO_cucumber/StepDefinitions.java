c3f097bf7bb10f524a0b70058a75c117f7a4ad45
Szabolcs Juhász <183376403+juhasz-szabolcs@users.noreply.github.com>
2026-01-15 21:23:24 +0100

    Step definiciók aktualizálása

--- src/test/java/HELLO_cucumber/StepDefinitions.java
+++ src/test/java/HELLO_cucumber/StepDefinitions.java
@@ -4,24 +4,29 @@ import io.cucumber.java.en.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return null;
+    }
+}
+
 public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
 
     @Given("today is Sunday")
     public void today_is_sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        today = "Sunday";
     }
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_be_told(String expectedAnswer) {
+        assertThat(actualAnswer).isEqualTo(expectedAnswer);
     }
 
 }
